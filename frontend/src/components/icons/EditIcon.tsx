23b8cd293242b5e143cf47f1942d50f131c0e27f
Anton Marusin <50703769+Toha4@users.noreply.github.com>
2026-01-05 21:48:46 +1000

    Исправлен ошибка иконки редактирования

--- frontend/src/components/icons/EditIcon.tsx
+++ frontend/src/components/icons/EditIcon.tsx
@@ -9,7 +9,7 @@ const EditIcon: React.FC<EditIconProps> = ({ className = 'w-5 h-5' }) => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       className={className}
-      viewBox="0 0 24"
+      viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeWidth="2"
